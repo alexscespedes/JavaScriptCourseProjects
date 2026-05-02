0c817eee1503c6674ad791e0fdb7c580a23d9a8d
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2026-05-02 10:12:49 -0400

    Challenge [#3] completed with updates of experts.

--- 16-Async/starter/challenge3.js
+++ 16-Async/starter/challenge3.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const wait = async function (seconds) {
-  return await new Promise(function (resolve) {
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
     setTimeout(resolve, seconds * 1000);
   });
 };
@@ -27,35 +27,37 @@ const createImage = function (imgPath) {
 const loadNPause = async function () {
   try {
     // Image 1
-    const img1 = await createImage('/img/img-1.jpg');
+    let img = await createImage('/img/img-1.jpg');
     console.log('Image 1 loaded');
     await wait(2);
     console.log('after async await 1');
-    img1.style.display = 'none';
+    img.style.display = 'none';
 
     // Image 2
-    const img2 = await createImage('/img/img-2.jpg');
+    img = await createImage('/img/img-2.jpg');
     console.log('Image 2 loaded');
     await wait(2);
     console.log('after async await 2');
-    img2.style.display = 'none';
-  } catch (error) {
-    console.error(error);
-    throw error;
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 };
 
 // loadNPause();
 
 const loadAll = async function (imgArr) {
-  const imgs = imgArr.map(img => createImage(img));
-  const data = await Promise.all(imgs);
-  data.map(img => {
-    img.classList.add('parallel');
-    console.log(img);
-  });
-  // const newData = data.map(img => img.classList.add('paralell'));
-  // newData.map(img => console.log(img));
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+
+    const data = await Promise.all(imgs);
+
+    data.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
 loadAll(['/img/img-1.jpg', '/img/img-2.jpg', '/img/img-3.jpg']);
